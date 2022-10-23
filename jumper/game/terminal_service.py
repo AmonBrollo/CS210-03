e752af79e56ec42fc3514b722b0926617a2a0f29
Amon Brollo <amonbrollo@gmail.com>
2022-10-23 12:13:55 -0300

    🖥 Added class with description

---
+++ jumper/game/terminal_service.py
@@ -0,0 +1,6 @@
+class TerminalService:
+    """A service that handles terminal operations.
+    
+    The responsibility of a TerminalService is to provide input and output operations for the 
+    terminal.
+    """
