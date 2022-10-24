24437a2b0354d2ee434908e364fd2d43fb4c0544
Amon Brollo <amonbrollo@gmail.com>
2022-10-24 09:08:59 -0300

    🖥 added display_jumper including description and functionality

--- jumper/game/terminal_service.py
+++ jumper/game/terminal_service.py
@@ -8,13 +8,21 @@ class TerminalService:
 
 
     def display_puzzle(puzzle):
-        """"Display the blank puzzle space according to the puzzle word
+        """Display the blank puzzle space according to the puzzle word.
         
         Args:
             puzzle (string): The Puzzle word.
-
         """
         blank_space = ""
         for i in len(puzzle):
             blank_space += "_"
         return blank_space
+
+    def display_jumper(phases, phase_number):
+        """Display the phase the jumper is in.
+
+        Args:
+            phase_number (int): A number to influence the level.
+            phases (list<string>): Representation of the Jumper in all its phases.
+        """
+        print(phases[phase_number])
