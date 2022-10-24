ffb0cb129d61fd9c9d7127760318643886d237dd
Amon Brollo <amonbrollo@gmail.com>
2022-10-23 21:14:47 -0300

    🖥 Added display_puzzle with decription and functionality

--- jumper/game/terminal_service.py
+++ jumper/game/terminal_service.py
@@ -4,3 +4,17 @@ class TerminalService:
     The responsibility of a TerminalService is to provide input and output operations for the 
     terminal.
     """
+
+
+
+    def display_puzzle(puzzle):
+        """"Display the blank puzzle space according to the puzzle word
+        
+        Args:
+            puzzle (string): The Puzzle word.
+
+        """
+        blank_space = ""
+        for i in len(puzzle):
+            blank_space += "_"
+        return blank_space
