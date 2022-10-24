2d22893faf1517ddea73721965e809350f64144c
Amon Brollo <amonbrollo@gmail.com>
2022-10-24 09:26:39 -0300

    🧩🔤 Added __init__ including description and _word

--- jumper/game/puzzle.py
+++ jumper/game/puzzle.py
@@ -9,3 +9,13 @@ class Puzzle:
     Attributes:
         _words (list<string>): A list of words 
     """
+
+    def __init__(self):
+        """Constructs a new Puzzle.
+        
+        Args:
+            self (Puzzle): An instance if Puzzle.
+        """
+        self._words = ["easy","photography","baby","episode","withdraw","qualified","watch","trustee",
+        "ghostwriter","thread","overeat","perfume","roar","impound","primary","listen","helmet",
+        "revolutionary","west","sister"]
