ffa64baeacfcbcb8ecf51d2d18cdbf0392a3b32c
Amon Brollo <amonbrollo@gmail.com>
2022-10-24 09:20:25 -0300

    🧩 Added description to the class

--- jumper/game/puzzle.py
+++ jumper/game/puzzle.py
@@ -2,3 +2,10 @@ import random
 
 
 class Puzzle:
+    """A random word generated for the player to guess.
+    
+    The responsibility of the Puzzle is to generate a random word for the player to guess and display the
+    puzzle being solved.
+    Attributes:
+        _words (list<string>): A list of words 
+    """
